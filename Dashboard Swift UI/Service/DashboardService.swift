ecc1afdd546b8c440e17655632a10d3474cb8e0e
Chethan J <chethantony42@gmail.com>
2023-06-23 10:23:19 +0530

    token removed

---
+++ Dashboard Swift UI/Service/DashboardService.swift
@@ -0,0 +1,47 @@
+//
+//  DashboardService.swift
+//  Dashboard Swift UI
+//
+//  Created by Chethan J on 21/06/2023.
+//
+
+import Foundation
+
+
+class DashboardService {
+    
+    let token: String = ""
+    
+    func fetchData() async throws -> DashboardModel {
+        
+        let url = URL(string: "https://api.inopenapp.com/api/v1/dashboardNew".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "GET"
+        request.setValue(
+            "Bearer \(token)",
+            forHTTPHeaderField: "Authentication"
+        )
+        
+        let sessionConfiguration = URLSessionConfiguration.default
+
+        sessionConfiguration.httpAdditionalHeaders = [
+            "Authorization": "Bearer \(token)"
+        ]
+
+        let session = URLSession(configuration: sessionConfiguration)
+
+        
+        let (data, response) = try await session.data(for: request)
+        print(response)
+        print(data)
+        guard response is HTTPURLResponse else {
+            fatalError("dashboard Api call failed")
+        }
+        
+        let fetchedData = try JSONDecoder().decode(DashboardModel.self, from: data)
+        print(fetchedData)
+        return fetchedData
+    }
+    
+}
